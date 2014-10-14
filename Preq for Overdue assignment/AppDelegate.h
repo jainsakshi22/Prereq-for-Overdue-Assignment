3b635e19130e39b11d2315e7771cea837b165d02
Sakshi Jain <sakshi@Sakshis-MacBook-Pro.local>
2014-10-14 21:22:22 +0530

    Initial Commit

---
+++ Preq for Overdue assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Preq for Overdue assignment
+//
+//  Created by Sakshi Jain on 14/10/14.
+//  Copyright (c) 2014 Sakshi Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Preq for Overdue assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Preq for Overdue assignment
+//
+//  Created by Sakshi Jain on 14/10/14.
+//  Copyright (c) 2014 Sakshi Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
